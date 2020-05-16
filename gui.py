979d35b059c5ee7fc43dabe792eaf4362e947e0f
Luka <luka.kresnik@hotmail.com>
2020-05-16 11:23:22 +0200

    Slike ne fittam v label, ampak vključim matplotlib v okno skupaj z toolbar vrstico (premikanje/povečevanje...)

--- gui.py
+++ gui.py
@@ -3,9 +3,16 @@
 from tkinter import *
 from tkinter import filedialog, messagebox
 from PIL import ImageTk, Image
+import PIL.Image as im
 import os
+import numpy as np
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.backend_bases import key_press_handler
 
 MainWindow = Tk()
+MainWindow.wm_title("Robotski vid - projekt čebelice")
 MainWindow.geometry("1280x900")
 MainWindow.resizable(width=True, height=True)
 # stranski menu z parametri ?
@@ -29,7 +36,7 @@ def select_file():
             if answer == "no":
                 break
         
-
+'''
 def open_img():
     filename = select_file()
     img = Image.open(filename)
@@ -38,8 +45,30 @@ def open_img():
     panel = Label(MainWindow, image=img)
     panel.image = img
     panel.pack()
+'''
+def open_img():
+    filename = select_file()
+    iImage = np.array(im.open(filename))
+
+    fig =plt.figure(figsize=(5,4), dpi=100)
+    plt.imshow(iImage)
+
+    canvas = FigureCanvasTkAgg(fig, master=mainarea)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=TOP, fill= BOTH, expand=1)
+
+    toolbar = NavigationToolbar2Tk(canvas, mainarea)
+    toolbar.update()
+    canvas.get_tk_widget(sidemenu=TOP, fill=BOTH, expand=1)
+
+
+def _quit():
+    MainWindow.quit()
+    MainWindow.destroy()
 
 btn = Button(sidemenu, text='Izberi sliko', command=open_img, width = 20, height = 3).place(x=120,y=50)
-
+    
+button = Button(master=mainarea, text="Quit", command=_quit)
+button.pack(side=BOTTOM)
 
 MainWindow.mainloop()
